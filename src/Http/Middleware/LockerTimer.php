a74d5d1784b42dc15b98ef0e360991478d561c87
orpheusohms <ominiabohsdavid@gmail.com>
2024-08-13 11:28:51 +0100

    add fallback number incase of misconfiguraton

--- src/Http/Middleware/LockerTimer.php
+++ src/Http/Middleware/LockerTimer.php
@@ -15,7 +15,7 @@ class LockerTimer
      */
     public function handle($request, Closure $next)
     {
-        if ($request->method() === 'GET' && $request->session()->get('locker_last_activity') && (time() - $request->session()->get('locker_last_activity')) > config('filament-lockscreen.activity_timeout')) {
+        if ($request->method() === 'GET' && $request->session()->get('locker_last_activity') && (time() - $request->session()->get('locker_last_activity')) > config('filament-lockscreen.activity_timeout', 0)) {
             $request->session()->put('lockscreen', true);
         }
 
